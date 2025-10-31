4bb20cdf98a7e0b7d3f23c33ff1b4388675d26f4
Creamcheesepie <gnldbs1004@naver.com>
2025-10-31 11:31:28 +0900

    1~5단계까지 완정

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,54 @@
 package com.back
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+var wiseSayings : MutableList<WiseSaying> = mutableListOf();
+var id : Int = 1;
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+class WiseSaying(val number : Int,val content : String,val author : String ){
+    fun print(){
+        println("$number / $author / $content")
     }
+}
+
+
+fun main() {
+    println("== 명언 앱 ==")
+
+    while(true) {
+        print("명령) ")
+        val input = readln();
+
+        when (input) {
+            "등록" -> saveWiseSaying();
+            "목록" -> wiseSayingList();
+            "종료" -> break;
+        }
+    }
+
+}
+
+fun saveWiseSaying(){
+    print("명언: ")
+    val content : String = readln();
+    print("작가: ")
+    val author : String = readln();
+    wiseSayings.add(WiseSaying(id++, content ,author));
+}
+
+fun wiseSayingList(){
+    println("번호    /    작가  /   명언")
+    for(wiseSaying in wiseSayings){
+        wiseSaying.print();
+    }
+}
+
+fun deleteWiseSaying(id : Int){
+    for(wiseSaying in wiseSayings){
+        if(wiseSaying.number == id){
+            wiseSayings.remove(wiseSaying);
+            return;
+        }
+    }
+
+    println("${id}번 명언은 존재하지 않습니다.");
 }
